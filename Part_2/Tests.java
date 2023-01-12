34b3c79208e476a2bb40d58660c13682fc0c317a
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2023-01-12 19:04:40 +0200

    Update Tests.java
    Added the tests from main

--- Part_2/Tests.java
+++ Part_2/Tests.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 import java.util.concurrent.*;
+
+import static java.lang.Thread.sleep;
+
 public class Tests {
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
 
     @Test
     public void partialTest() {
-        CustomExecutor customExecutor = new CustomExecutor();
+        CustomExecutor service = new CustomExecutor();
         var task = Task.createTask(() -> {
             int sum = 0;
             for (int i = 1; i <= 10; i++) {
@@ -17,7 +20,7 @@ public class Tests {
             }
             return sum;
         }, TaskType.COMPUTATIONAL);
-        Future<Integer> sumTask = customExecutor.submit(task);
+        Future<Integer> sumTask = service.submit(task);
         final int sum;
         try {
             sum =  sumTask.get(1, TimeUnit.MILLISECONDS);
@@ -33,10 +36,10 @@ public class Tests {
             return sb.reverse().toString();
         };
         // var is used to infer the declared type automatically
-        var priceTask = customExecutor.submit(() -> {
+        var priceTask = service.submit(() -> {
             return 1000 * Math.pow(1.02, 5);
         }, TaskType.COMPUTATIONAL);
-        var reverseTask = customExecutor.submit(callable2, TaskType.IO);
+        var reverseTask = service.submit(callable2, TaskType.IO);
         final Double totalPrice;
         final String reversed;
         try {
@@ -48,7 +51,434 @@ public class Tests {
         logger.info(() -> "Reversed String = " + reversed);
         logger.info(() -> String.valueOf("Total Price = " + totalPrice));
         logger.info(() -> "Current maximum priority = " +
-                customExecutor.getCurrentMaxPriority());
-        customExecutor.gracefullyTerminate();
+                service.getCurrentMaxPriority());
+        CustomFuture<Integer> tasks = service.submit(() -> {
+            int sums = 0;
+            for (int i = 0; i < 100000; i++) {
+                sums+= i;
+            }
+            return sums;
+        });
+        try {
+            int test = tasks.get(1, TimeUnit.MILLISECONDS);
+            System.out.println(test + " hey");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        Future<String> task2 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        Future<String> task3 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        Future<String> task4 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+
+        Future<String> task25 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        Future<String> task24 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        Future<String> task23 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        System.out.println(service.toString());
+        Future<String> task22 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        Future<String> task21 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        System.out.println(service.toString());
+        Future<String> task20 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        try {
+            sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Future<String> task5 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        Future<String> task6 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        }, TaskType.IO);
+
+        Future<String> task8 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        }, TaskType.IO);
+
+        Future<String> task9 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        }, TaskType.IO);
+        System.out.println(service.toString());
+        Future<String> task10 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        }, TaskType.IO);
+
+        Future<String> task11 = service.submit(() -> {
+            sleep(200);
+            System.out.println("sleep task");
+            return "sleep task";
+        });
+        System.out.println(service.toString());
+
+        Future<String> task12 = service.submit(() -> {
+
+            System.out.println("sleep task this");
+            return "sleep task";
+        }, TaskType.IO);
+        try {
+            sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(service.toString());
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, TaskType.COMPUTATIONAL);
+        System.out.println(service.toString());
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println(service.toString());
+        service.submit(() -> {
+            try {
+                sleep(1090);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        try {
+            sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        service.submit(() -> {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        service.submit(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        System.out.println(service.toString());
+        service.submit(() -> System.out.println("hello from runnable"));
+        Future<String> task7 = service.submit(() -> {
+            sleep(200);
+            System.out.println("special sleep task");
+            return "special sleep task";
+        }, TaskType.IO);
+        System.out.println(service.toString());
+
+
+        service.gracefullyTerminate();
+        try {
+            sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(service.toString());
+
+
+        System.out.println(service.getCurrentMaxPriority());
+        System.out.println(task.toString());
+        service.gracefullyTerminate();
     }
 }
